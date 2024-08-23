4084e58c7837f57f9007619ca476df9be3d31b31
Sima3443 <toppochka@gmail.com>
2024-08-23 22:44:15 +0300

    📊stats

--- app/handlers/admin/stats.py
+++ app/handlers/admin/stats.py
@@ -5,15 +5,25 @@ from loader import dp, bot
 
 from data.config import DIR
 
+from database.service.stats import get_profile_stats, get_users_stats
+
+from utils.graphs import create_user_invite_graph
+
 photo_path = rf'{DIR}/photo/invites_per_user.png'
 
 
 @dp.message_handler(Command("stats"))
 async def _stats_command(message: types.Message):
-    from utils.graphs import create_user_invite_graph
+
     create_user_invite_graph(photo_path)
+    user_stats = get_profile_stats()
+
+    text = f"👤Users: {get_users_stats()}\n\nProfiles: {user_stats['total_users']}\n🙍‍♂️Man: {user_stats['male_users']} | 🙍‍♀️Woman: {user_stats['female_users']}"
     with open(photo_path, "rb") as photo:
-        await message.answer_photo(photo)
+        await message.answer_photo(
+            photo,
+            text
+            )
 
 
 
--- database/service/stats.py
+++ database/service/stats.py
@@ -1,7 +1,9 @@
-from ..models.users import Users
 from utils.logging import logger
-from peewee import fn
 
+from ..models.users import Users
+from ..models.profile import Profile
+
+from peewee import fn, Case
 
 def get_users_invite():
     top_users = (Users
@@ -11,3 +13,15 @@ def get_users_invite():
     users = [user.username for user in top_users]
     invites = [user.referral for user in top_users]
     return users, invites
+
+
+def get_users_stats():
+    return Users.select().count()
+
+def get_profile_stats():
+    query = Profile.select(
+    fn.COUNT(Profile.id).alias('total_users'),
+    fn.SUM(Case(None, [(Profile.gender == 'male', 1)], 0)).alias('male_users'),
+    fn.SUM(Case(None, [(Profile.gender == 'female', 1)], 0)).alias('female_users')
+    )
+    return query.dicts().get()
