ed39412f991eb5bacf3f9a756f58d7387c6c82ff
devvsima <toppochka@gmail.com>
2025-09-08 17:28:36 +0300

    Fix default language

--- app/middlewares/i18n.py
+++ app/middlewares/i18n.py
@@ -26,7 +26,11 @@ from loader import i18n
 class MyI18nMiddleware(I18nMiddleware):
     async def get_locale(self, event: Update, data: dict) -> str:
         user = data.get("user")
-        return user.language if user else await super().get_locale(event, data)
+        if user and user.language:
+            if user.language in i18n.available_locales:
+                return user.language
+            return "en"
+        return await super().get_locale(event, data)
 
 
 i18n_middleware = MyI18nMiddleware(i18n)
