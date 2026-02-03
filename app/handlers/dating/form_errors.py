b2e3ceaea0c8d84313d10d7bf0c47ad8a95754b5
Maksym <toppochka@gmail.com>
2026-02-03 18:14:52 +0200

    Fixed a bug where the bot did not respond to incorrectly filled out questionnaires.

--- app/handlers/dating/form_errors.py
+++ app/handlers/dating/form_errors.py
@@ -1,48 +1,60 @@
 from aiogram import types
 from aiogram.filters.state import StateFilter
 
-from app.routers import dating_router
+from app.routers import dating_router, registration_router
 from app.states.default import ProfileCreate, ProfileEdit
 from app.text import message_text as mt
 
 
-@dating_router.message(StateFilter(ProfileCreate.gender))
+@registration_router.message(StateFilter(ProfileCreate.gender))
 async def _incorrect_gender(message: types.Message):
     """Ошибка фильтра гендера"""
     await message.answer(mt.INVALID_RESPONSE)
 
 
-@dating_router.message(StateFilter(ProfileCreate.find_gender))
+@registration_router.message(StateFilter(ProfileCreate.find_gender))
 async def _incorrect_find_gender(message: types.Message):
     """Ошибка фильтра гендера"""
     await message.answer(mt.INVALID_RESPONSE)
 
 
-@dating_router.message(StateFilter(ProfileCreate.photo, ProfileEdit.photo))
-async def _incorrect_photo(message: types.Message):
-    """Ошибка фильтра фото"""
+@registration_router.message(StateFilter(ProfileCreate.photo))
+async def _incorrect_photo_create(message: types.Message):
+    """Ошибка фильтра фото при создании"""
     await message.answer(mt.INVALID_PHOTO)
 
 
-@dating_router.message(StateFilter(ProfileCreate.name))
+@dating_router.message(StateFilter(ProfileEdit.photo))
+async def _incorrect_photo_edit(message: types.Message):
+    """Ошибка фильтра фото при редактировании"""
+    await message.answer(mt.INVALID_PHOTO)
+
+
+@registration_router.message(StateFilter(ProfileCreate.name))
 async def _incorrect_name(message: types.Message):
     """Ошибка фильтра имени"""
     await message.answer(mt.INVALID_NAME)
 
 
-@dating_router.message(StateFilter(ProfileCreate.age))
+@registration_router.message(StateFilter(ProfileCreate.age))
 async def _incorrect_age(message: types.Message):
     """Ошибка фильтра возраста"""
     await message.answer(mt.INVALID_AGE)
 
 
-@dating_router.message(StateFilter(ProfileCreate.city))
+@registration_router.message(StateFilter(ProfileCreate.city))
 async def _incorrect_city(message: types.Message):
     """Ошибка фильтра города"""
     await message.answer(mt.INVALID_CITY_RESPONSE)
 
 
-@dating_router.message(StateFilter(ProfileCreate.description, ProfileEdit.description))
-async def _incorrect_description(message: types.Message):
-    """Ошибка фильтра описания"""
+@registration_router.message(StateFilter(ProfileCreate.description))
+async def _incorrect_description_create(message: types.Message):
+    """Ошибка фильтра описания при создании"""
+    await message.answer(mt.INVALID_DESCIPTION)
+
+
+@registration_router.message(StateFilter(ProfileEdit.description))
+async def _incorrect_description_edit(message: types.Message):
+    """Ошибка фильтра описания при редактировании"""
     await message.answer(mt.INVALID_DESCIPTION)
